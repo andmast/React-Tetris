ac9d934060db6e2ff78d04476088e68e4d5e3680
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-25 23:32:16 -0500

    added collison chcks to Tetris component functions

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+//Helpers
+import { createStage, checkCollision } from "../gameHelpers";
+
 // Components
 import Stage from "./Stage";
 import Display from "./Display";
@@ -11,29 +14,41 @@ import { useStage } from "../hooks/useStage";
 
 // Styles
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
-import { createStage } from "../gameHelpers";
 
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("re-render");
 
   const movePlayer = dir => {
-    updatePlayerPos({ x: dir, y: 0 });
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
   };
 
   const startGame = () => {
-    //Reset everything
+    // Reset everything
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   };
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1 });
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game Over
+      if (player.pos.y < 1) {
+        console.log("GAME OVER!!!");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
   };
 
   const dropPlayer = () => {
